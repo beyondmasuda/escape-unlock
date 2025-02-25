c27a1833f021ad62819517a391562e8452f8baca
beyondmasuda <jun_masuda@beyondjapan.com>
2025-02-26 04:11:05 +0900

    fix: set basePath and assetPrefix for consistent routing in Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: '/escape-unlock',
+  assetPrefix: '/escape-unlock',
+};
 
 export default nextConfig;
